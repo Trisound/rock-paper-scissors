276daa361cd634cf429d1150fd666329c71b21d2
Trisound <hethinksthrice@gmail.com>
2025-08-28 21:34:50 +0900

    added getHumanChoice function.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,9 +1,27 @@
 
 function getComputerChoice(number){
-         Math.random();
-    
-         return number
+         
+      return Math.random(number)
         
 }
 
-console.log(number);
+console.log((getComputerChoice()));
+
+ 
+
+function getHumanChoice(String){
+ const choiceButton = document.querySelector("#choiceButton");
+ const log = document.querySelector("#log");
+   String = ""
+ choiceButton.addEventListener("click", () => {
+   let choice = prompt("Whats your choice?");
+   if ( choice === null){
+    log.innerText = "I need a choice!";
+   } else if (String > 0) {
+      choice = "Rock wins!";
+   }
+   });
+
+    return prompt("Please choose rock, paper, or scissors!")
+}
+console.log(getHumanChoice(String));
