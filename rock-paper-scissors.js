6d94071e31615334fc3d99f7a7a9071e4ce56a3c
Trisound <hethinksthrice@gmail.com>
2025-09-02 21:45:48 +0900

    Added Switch conditions, Added result Function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -34,6 +34,46 @@ function getHumanChoice(){
       log.innerText = "No choice";
 }
 
-    return prompt("Please choose rock, paper, or scissors!")})
+    return prompt("No choice has been chosen!")})
 }
 console.log(getHumanChoice());
+
+ 
+function result(getHumanChoice,getComputerChoice){
+   switch (getHumanChoice){
+ case "Rock":
+   if (getComputerChoice === "Scissors")
+      return "You win! Rock beats Scissors.";
+    else if (getComputerChoice === "Paper"){
+      return "You lose Paper beats Rock!";
+   } else {
+      return " It is a tie!"
+   }
+ case "Rock":
+   if (getComputerChoice === "Scissors")
+      return "You win! Rock beats Scissors.";
+    else if (getComputerChoice === "Paper"){
+      return "You lose Paper beats Rock!";
+   } else {
+      return " It is a tie!"
+   }
+ case "Paper":
+   if (getComputerChoice === "Rock")
+      return "You win! Paper beats Rock.";
+    else if (getComputerChoice === "Scissors"){
+      return "You lose Scissors beats Paper!";
+   } else {
+      return " It is a tie!"
+   }
+ case "Scissors":
+   if (getComputerChoice === "Paper")
+      return "You win! Rock beats Scissors.";
+    else if (getComputerChoice === "Rock"){
+      return "You lose Rock beats Scissors!";
+   } else {
+      return " It is a tie!"
+   }
+}
+ 
+
+console.log(result())}
