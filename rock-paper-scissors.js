c002279ad5eb8d2dd08ab02983023c8387501694
Trisound <hethinksthrice@gmail.com>
2025-09-04 21:33:54 +0900

    Added result function in event listener. Added defult in result function. Deleted prompt and condtions in getHumanChoice function.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,79 +1,69 @@
 
-function getComputerChoice(){
-          let number = Math.random();
-         if (number < 0.33){
-            return "Rock"
-         } else if (number < 0.66){
-            return "Paper"
-         } else {
-            return "Scissor";
-         }
-      
-        
+function getComputerChoice() {
+   let number = Math.random();
+   if (number < 0.33) {
+      return "Rock"
+   } else if (number < 0.66) {
+      return "Paper"
+   } else {
+      return "Scissor";
+   }
+
+
 }
 
 console.log((getComputerChoice()));
 
- 
 
-function getHumanChoice(){
- const choiceButton = document.querySelector("#choiceButton");
- const log = document.querySelector("#log");
 
- choiceButton.addEventListener("click", () => {
-   let choice = prompt("Whats your choice?");
-   if ( choice === null){
-    log.innerText = "I need a choice!";
-   } else if (choice === "Rock") {
-      log.innerText = "Rock wins!"
-   } else if (choice === "Paper") {
-      log.innerText = "Paper wins!"
-   } else if (choice === "Scissors") {
-      log.innerText = "Scissors wins!"
-   } else {choice === null
-      log.innerText = "No choice";
-}
+function getHumanChoice() {
+   const choiceButton = document.querySelector("#choiceButton");
+   const log = document.querySelector("#log");
 
-    return prompt("No choice has been chosen!")})
+   choiceButton.addEventListener("click", () => {
+      let choice = prompt("Whats your choice?");
+
+      alert(result(choice, getComputerChoice()));
+      
+   })
+
 }
 console.log(getHumanChoice());
 
- 
-function result(getHumanChoice,getComputerChoice){
-   switch (getHumanChoice){
- case "Rock":
-   if (getComputerChoice === "Scissors")
-      return "You win! Rock beats Scissors.";
-    else if (getComputerChoice === "Paper"){
-      return "You lose Paper beats Rock!";
-   } else {
-      return " It is a tie!"
-   }
- case "Rock":
-   if (getComputerChoice === "Scissors")
-      return "You win! Rock beats Scissors.";
-    else if (getComputerChoice === "Paper"){
-      return "You lose Paper beats Rock!";
-   } else {
-      return " It is a tie!"
-   }
- case "Paper":
-   if (getComputerChoice === "Rock")
-      return "You win! Paper beats Rock.";
-    else if (getComputerChoice === "Scissors"){
-      return "You lose Scissors beats Paper!";
-   } else {
-      return " It is a tie!"
-   }
- case "Scissors":
-   if (getComputerChoice === "Paper")
-      return "You win! Rock beats Scissors.";
-    else if (getComputerChoice === "Rock"){
-      return "You lose Rock beats Scissors!";
-   } else {
-      return " It is a tie!"
-   }
-}
- 
 
-console.log(result())}
+
+function result(getHumanChoice, getComputerChoice) {
+
+   console.log((getHumanChoice + getComputerChoice))
+
+   switch (getHumanChoice) {
+      case "Rock":
+         if (getComputerChoice === "Scissors")
+            return "You win! Rock beats Scissors.";
+         else if (getComputerChoice === "Paper") {
+            return "You lose Paper beats Rock!";
+         } else {
+            return " It is a tie!"
+         }
+
+      case "Paper":
+         if (getComputerChoice === "Rock")
+            return "You win! Paper beats Rock.";
+         else if (getComputerChoice === "Scissors") {
+            return "You lose Scissors beats Paper!";
+         } else {
+            return " It is a tie!"
+         }
+      case "Scissors":
+         if (getComputerChoice === "Paper")
+            return "You win! Rock beats Scissors.";
+         else if (getComputerChoice === "Rock") {
+            return "You lose Rock beats Scissors!";
+         } else {
+            return " It is a tie!"
+         }
+      default:
+         return "Invalid input. Please use 'Rock', 'Paper', 'Scissors'."
+   }
+
+}
